e77f6a079d14ba4ffbb63e57ea2f55114e5443fb
Anvar571 <abdurashidovanvar52@gmail.com>
2025-03-10 00:12:29 +0500

    Basket is completed

--- src/modules/basket/dto/index.ts
+++ src/modules/basket/dto/index.ts
@@ -1,17 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsNumber, Min } from 'class-validator';
 
 export class AddToBasketDto {
+  @ApiProperty()
   @IsNumber()
   productId: number;
 
+  @ApiProperty()
   @IsNumber()
   quantity: number;
 }
 
 export class UpdateBasketDto {
+  @ApiProperty()
   @IsInt()
   basketId: number;
 
+  @ApiProperty()
   @IsInt()
   @Min(1)
   quantity: number;
