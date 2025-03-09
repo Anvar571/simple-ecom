eb8d361ed6e47ea08a5c13527587f964dcb91253
Anvar571 <abdurashidovanvar52@gmail.com>
2025-03-10 00:13:58 +0500

    other changes

--- src/modules/products/dtos/index.ts
+++ src/modules/products/dtos/index.ts
@@ -1,46 +1,59 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNumber, IsOptional } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty()
   @IsString()
   name: string;
 
+  @ApiProperty()
   @IsString()
   description: string;
 
+  @ApiProperty()
   @IsNumber()
   price: number;
 
+  @ApiProperty()
   @IsNumber()
   stock: number;
 
+  @ApiProperty()
   @IsNumber()
   categoryId: number;
 
+  @ApiProperty()
   @IsString()
   image: string;
 }
 
 export class UpdateProductDto {
+  @ApiProperty()
   @IsString()
   @IsOptional()
   name?: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty()
   @IsNumber()
   @IsOptional()
   price?: number;
 
+  @ApiProperty()
   @IsNumber()
   @IsOptional()
   stock?: number;
 
+  @ApiProperty()
   @IsNumber()
   @IsOptional()
   categoryId?: number;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   image?: string;
